ec15f109bd445a7e10cef6542714998221edd9d2
quangtuanitmo18 <quangtuanitmo18@gmail.com>
2024-06-13 18:17:30 +0300

    chore: minor issue

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,7 @@ const requestCounter = new Counter({
 
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
-  max: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  max: 10, // Limit each IP to 10 requests per `window` (here, per 1 minute)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   statusCode: HttpStatusCode.TooManyRequests
